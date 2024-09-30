5f079f8559e88f5bbed8ae85a90485752bf7169e
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-01 01:13:36 +0300

    Add first view class

--- air_service/views.py
+++ air_service/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from air_service.models import Country
+from air_service.serializers import CountrySerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    model = Country
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
