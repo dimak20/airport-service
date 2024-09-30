283113a13479a127ed6b667c9e3dd6032d1669b2
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-01 01:13:24 +0300

    Add rest_framework to installed_apps

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #3d party
+    "rest_framework",
     #my apps
     "air_service",
     "user"
