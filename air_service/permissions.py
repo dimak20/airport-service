33e42d8bfb5bb05e6e9c89f61b1a194494f92534
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-02 22:51:35 +0300

    Fix permission doc

--- air_service/permissions.py
+++ air_service/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminAllORIsAuthenticatedOrReadOnly(BasePermission):
     """
-    Provide full permission to admin user or permissions if safe methods
+    Provides full permission to admin user or permissions if safe methods
     """
 
     def has_permission(self, request, view):
