67f6fec3a152c90cf5d02b795b5bed52954a696b
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-02 19:59:00 +0300

    Flake8 filters.py

--- air_service/filters.py
+++ air_service/filters.py
@@ -3,72 +3,62 @@ from django.db.models.functions import TruncHour
 
 
 class RouteFilter(django_filters.FilterSet):
-    distance_min = django_filters.NumberFilter(
-        field_name="distance", lookup_expr="gte"
-    )
-    distance_max = django_filters.NumberFilter(
-        field_name="distance", lookup_expr="lte"
-    )
-    name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
+    distance_min = django_filters.NumberFilter(field_name="distance", lookup_expr="gte")
+    distance_max = django_filters.NumberFilter(field_name="distance", lookup_expr="lte")
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     source_city = django_filters.CharFilter(
-        field_name="source__closest_big_city__name",
-        lookup_expr="icontains"
+        field_name="source__closest_big_city__name", lookup_expr="icontains"
     )
     destination_city = django_filters.CharFilter(
-        field_name="destination__closest_big_city__name",
-        lookup_expr="icontains"
+        field_name="destination__closest_big_city__name", lookup_expr="icontains"
     )
 
+
 class FlightFilter(django_filters.FilterSet):
-    route_ids = django_filters.CharFilter(method='filter_by_route_ids')
+    route_ids = django_filters.CharFilter(method="filter_by_route_ids")
     airplane_name = django_filters.CharFilter(
         field_name="airplane__name", lookup_expr="icontains"
     )
-    departure_time_hour = django_filters.DateTimeFilter(
-        method='filter_by_hour'
-    )
+    departure_time_hour = django_filters.DateTimeFilter(method="filter_by_hour")
     departure_time_hour_after = django_filters.DateTimeFilter(
-        method='filter_by_hour_gte'
+        method="filter_by_hour_gte"
     )
     departure_time_hour_before = django_filters.DateTimeFilter(
-        method='filter_by_hour_lte'
+        method="filter_by_hour_lte"
     )
 
     def filter_by_hour(self, queryset, name, value):
-        return queryset.annotate(
-            departure_hour=TruncHour('departure_time')
-        ).filter(departure_hour=value)
+        return queryset.annotate(departure_hour=TruncHour("departure_time")).filter(
+            departure_hour=value
+        )
 
     def filter_by_hour_gte(self, queryset, name, value):
-        return queryset.annotate(
-            departure_hour=TruncHour('departure_time')
-        ).filter(departure_hour__gte=value)
+        return queryset.annotate(departure_hour=TruncHour("departure_time")).filter(
+            departure_hour__gte=value
+        )
 
     def filter_by_hour_lte(self, queryset, name, value):
-        return queryset.annotate(
-            departure_hour=TruncHour('departure_time')
-        ).filter(departure_hour__lte=value)
+        return queryset.annotate(departure_hour=TruncHour("departure_time")).filter(
+            departure_hour__lte=value
+        )
 
     def filter_by_route_ids(self, queryset, name, value):
         if value:
-            route_ids = [int(i) for i in value.split(',')]
+            route_ids = [int(i) for i in value.split(",")]
             return queryset.filter(route__id__in=route_ids)
         return queryset
 
+
 class CountryFilter(django_filters.FilterSet):
-    country_name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
+    country_name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+
 
 class CityFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     country_name = django_filters.CharFilter(
         field_name="country__name", lookup_expr="icontains"
     )
+
 
 class CrewFilter(django_filters.FilterSet):
     first_name = django_filters.CharFilter(
@@ -78,30 +68,22 @@ class CrewFilter(django_filters.FilterSet):
         field_name="last_name", lookup_expr="icontains"
     )
 
+
 class AirplaneTypeFilter(django_filters.FilterSet):
-    search_name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
+    search_name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+
 
 class AirportFilter(django_filters.FilterSet):
-    search_name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
+    search_name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     closest_city_name = django_filters.CharFilter(
-        field_name="closest_big_city__name",
-        lookup_expr="icontains"
+        field_name="closest_big_city__name", lookup_expr="icontains"
     )
 
+
 class AirplaneFilter(django_filters.FilterSet):
-    search_name = django_filters.CharFilter(
-        field_name="name", lookup_expr="icontains"
-    )
-    rows_min = django_filters.NumberFilter(
-        field_name="rows", lookup_expr="gte"
-    )
-    rows_max = django_filters.NumberFilter(
-        field_name="rows", lookup_expr="lte"
-    )
+    search_name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+    rows_min = django_filters.NumberFilter(field_name="rows", lookup_expr="gte")
+    rows_max = django_filters.NumberFilter(field_name="rows", lookup_expr="lte")
     seats_in_row_min = django_filters.NumberFilter(
         field_name="seats_in_row", lookup_expr="gte"
     )
@@ -109,10 +91,8 @@ class AirplaneFilter(django_filters.FilterSet):
         field_name="seats_in_row", lookup_expr="lte"
     )
     airplane_type_name = django_filters.CharFilter(
-        field_name="airplane_type__name",
-        lookup_expr="icontains"
+        field_name="airplane_type__name", lookup_expr="icontains"
     )
     crew_person_last_name = django_filters.CharFilter(
         field_name="crew__last_name", lookup_expr="iexact"
     )
-
