e343d3f620c2915ca6fdccac628218edb981f1c6
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-02 19:59:09 +0300

    Flake8 ordering.py

--- air_service/ordering.py
+++ air_service/ordering.py
@@ -6,14 +6,13 @@ class AirServiceOrdering:
     default_param = "-pk"
 
     @classmethod
-    def get_ordering_fields(
-        cls, request: Request, fields: list[str]
-    ) -> list[str]:
+    def get_ordering_fields(cls, request: Request, fields: list[str]) -> list[str]:
         ordering = request.query_params.get(cls.ordering_param, cls.default_param)
         ordering_fields = ordering.split(",")
         all_fields = set(["-" + field for field in fields] + fields)
-        processed_ordering_fields = [field for field in ordering_fields if
-                                     field in all_fields]
+        processed_ordering_fields = [
+            field for field in ordering_fields if field in all_fields
+        ]
         if not processed_ordering_fields:
             return [cls.default_param]
         return processed_ordering_fields
