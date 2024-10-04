277a76df67fa60f09e325341312a0947782b99e0
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-04 17:56:37 +0300

    Add default Docker image for project

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/files/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
