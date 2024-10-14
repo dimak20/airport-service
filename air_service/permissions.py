c8a23406315f792b6809d8d14d9bb12da91cfd2d
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-14 12:34:11 +0300

    Remove request.user condition

--- air_service/permissions.py
+++ air_service/permissions.py
@@ -7,9 +7,7 @@ class IsAdminAllORIsAuthenticatedOrReadOnly(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return bool(
-            request.method in SAFE_METHODS and request.user and
-            request.user.is_authenticated
-        ) or (
-            request.user and request.user.is_staff
-        )
+        return (
+                request.method in SAFE_METHODS and
+                request.user.is_authenticated
+        ) or request.user.is_staff
